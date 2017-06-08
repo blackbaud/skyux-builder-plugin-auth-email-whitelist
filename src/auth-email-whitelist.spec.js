b73392f2dc1b5b73a71e939c0f800f8952255248
Brandon Hare <blackbaud-brandonhare@users.noreply.github.com>
2017-06-08 13:30:00 -0400

    Updated tests

--- src/auth-email-whitelist.spec.js
+++ src/auth-email-whitelist.spec.js
@@ -14,10 +14,12 @@ describe('Auth Email Whitelist plugin', () => {
   it('should only change the content of the app-extras.module.ts file', () => {
     let content = '<p></p>';
     let path = 'sample.service.ts';
-    let skyAppConfig = {};
+    let skyAppConfig = {
+      skyux: {}
+    };
     let result = plugin.preload(content, path, skyAppConfig);
     expect(result).toBe(content);
-    skyAppConfig.auth = true;
+    skyAppConfig.skyux['auth'] = true;
     content = '{}';
     path = 'app-extras.module.ts';
     result = plugin.preload(content, path, skyAppConfig);
@@ -28,8 +30,10 @@ describe('Auth Email Whitelist plugin', () => {
     let content = '<p></p>';
     let path = 'app-extras.module.ts';
     let skyAppConfig = {
-      auth: false
-    };
+        skyxux:{
+          auth: true
+        }
+    }
     let result = plugin.preload(content, path, skyAppConfig);
     expect(result).toBe(content);
 
@@ -42,7 +46,9 @@ describe('Auth Email Whitelist plugin', () => {
     let content = '';
     let path = 'app-extras.module.ts';
     let skyAppConfig = {
-      auth: true
+        skyxux:{
+          auth: true
+        }
     }
     let result = plugin.preload(content, path, skyAppConfig);
     expect(result).toContain('(SkyAppBootstrapper as any).processBootstrapConfig');
